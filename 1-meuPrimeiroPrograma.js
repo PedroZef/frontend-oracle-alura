449157d0041100a4f5a34e51f2d8c81536c81664
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-06-10 14:59:21 -0300

    Meu Primeiro Programa JS:Trabalhando com variáveis!  first commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro Pragram JS: trabalhando com variáveis!")
+
+const idade = 61
+
+console.log('idade')
+console.log(idade)
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+2
+console.log(idadeSomada)
