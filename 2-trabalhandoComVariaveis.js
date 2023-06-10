9de6c35fd0885df4cdd15f44cc4e5535246a6ec5
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-06-10 19:43:16 -0300

    Adicionado arquivos Operações Aritmétricas, Conversão de Tipos

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+console.log('trabalhando com variáveis');
+// JS é case sensitive
+
+
+const idade = 61;
+const nome = "Pedro Zeferino";
+console.log(idade);
+console.log(nome);
+
+//não fazer
+// mes = "Janeiro"
---
+++ 4-conversaoDetipos.js
@@ -0,0 +1,8 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2023);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10 "/ "2");
+console.log("pedro" / "2"); // NaN Not a Number
